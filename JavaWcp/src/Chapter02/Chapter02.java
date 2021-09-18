1410c5fa5dc69aa7f27c23f63a1f27d51a8b3b04
shotaro0619 <66shotaro99@gmail.com>
2021-09-19 08:54:20 +0900

    確認

--- JavaWcp/src/Chapter02/Chapter02.java
+++ JavaWcp/src/Chapter02/Chapter02.java
@@ -2,6 +2,6 @@ package Chapter02;
 
 public class Chapter02 {
 	public static void main(String[] args) {
-        System.out.println("こんにちは！");
+        System.out.println("こんにちは！!!!!!!");
     }
 }
