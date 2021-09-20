88565c53242c196b6050d7fa85a1a9271bcb4f4a
shotaro0619 <66shotaro99@gmail.com>
2021-09-20 09:58:40 +0900

    [Finish]7章

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,23 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long2 = 29292929292929L;
+		System.out.println(long2);
+		double double1 = 1111111.111;
+		System.out.println(double1);
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		
+		Float f = 0.12345f;
+		System.out.println(f);
+		
+		Integer int1 = Integer.valueOf(10);
+		System.out.println(int1);
+		
+		Character c = 'A';
+		System.out.println(c);
+		
+		int[] array = new int[] {1, 2, 3, 4, 5};
+		System.out.println(array[3]);
+	}
+}
