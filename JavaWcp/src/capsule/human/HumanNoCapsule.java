b8a93911bb97cf379a751df9c7b6998cd96a2d26
shotaro0619 <66shotaro99@gmail.com>
2021-09-27 09:57:42 +0900

    chapter12

---
+++ JavaWcp/src/capsule/human/HumanNoCapsule.java
@@ -0,0 +1,12 @@
+package capsule.human;
+
+public class HumanNoCapsule {
+	public String name;
+	public int age;
+	
+	public HumanNoCapsule(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+}
---
+++ JavaWcp/src/practice/CapsuleMan.java
@@ -0,0 +1,24 @@
+package practice;
+
+public class CapsuleMan {
+  private String name;
+  private int age;
+
+	public CapsuleMan(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
